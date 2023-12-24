14706f0bc0500b7a783ba144560fd5e6435da4cf
ohts1031ECW <ohashitamc_main@outlook.jp>
2023-12-24 14:04:26 +0900

    testなんてねぇよ

---
+++ src/test/test.js
@@ -0,0 +1,24 @@
+//@ts-nocheck
+const scanner = World.getWorldScanner()
+.withBlockFilter("getId").contains("chest")
+.build()
+const rawdate = scanner.scanAroundPlayer(5).toString();
+const array = rawdate.replace(/[|]/g,"").split(",")
+
+Chat.log(rawdate);
+Chat.log(typeof rawdate)
+Chat.log(array)
+Chat.log(typeof array)
+//let out = [];
+for(const index in rawdate.length){
+    Chat.log(`index: ${index}`);
+    Chat.log(`item: ${rawdate[index]}`)
+}
+/*
+.withBlockFilter("getHardness").is("<=", 10)
+.andStringBlockFilter().contains("chest", "barrel")
+.withStringStateFilter().contains("facing=south")
+.andStateFilter("isToolRequired").is(false)
+.build()
+*/
+//Chat.log(out)
