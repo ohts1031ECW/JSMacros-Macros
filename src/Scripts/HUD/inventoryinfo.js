ce5d05da8eecf1eafc5a41664db7b92cc3dedc3f
ohts1031ECW <ohashitamc_main@outlook.jp>
2023-12-27 18:03:34 +0900

    backupo

--- src/Scripts/HUD/inventoryinfo.js
+++ src/Scripts/HUD/inventoryinfo.js
@@ -1,3 +1,26 @@
 const { ToggleScript } = require("../../lib/ohts-Lib");
 
-ToggleScript
+ToggleScript("inventoryinfo-dev",async()=>{
+    //Iscreen
+    const X = 100;
+    const Y = 100;
+    
+    Client.waitTick(10);
+    if(Hud.isContainer() === true){
+        //アイテム取得
+        //const contained_Item = Player.openInventory().getItems();
+
+        const Screen = Hud.getOpenScreen();///とりあえず仮で中に何が入ってるか
+        const ScreenName = Hud.getOpenScreenName();
+
+        Chat.log(`ScreenName: ${ScreenName}`);
+
+        //note white = 0xffffff
+        //Chat.log(`get title text: ${Screen?.getTitleText()}`);
+        //Screen?.textFieldBuilder().createWidget().setText("TEEEEEEEEEEEEExt").setActive(true)//.setPos(100,100);
+        Screen?.addText("めっちゃ明るくなったわ",X,Y,0xffffff,true);
+        //アイテム取得どうしましょ
+
+    
+    }
+})
