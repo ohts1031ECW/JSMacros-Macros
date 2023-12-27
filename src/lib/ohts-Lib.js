562072ed8a49d92b07a8bab65ff6ea8a5a361927
ohts1031ECW <ohashitamc_main@outlook.jp>
2023-12-27 17:03:23 +0900

    欠陥があったため修正

--- src/lib/ohts-Lib.js
+++ src/lib/ohts-Lib.js
@@ -1,19 +1,12 @@
 
+const defcolor = {
+    script: { bracket: 0x7, name: 0x5 },
+}
 /**
  * @param {string} ScriptName
  * @param {() => void} callback
- * @param {{ 
- * script: {bracket: number,name:number}} | undefined} color
  */
-function ToggleScript(ScriptName, callback,color) {
-    if(color === undefined){
-        color = {
-            script: {
-                bracket: 0x7,
-                name: 0x5
-            },
-        }
-    }
+function ToggleScript(ScriptName, callback, color = defcolor) {
 
 
 
@@ -22,7 +15,7 @@ function ToggleScript(ScriptName, callback,color) {
     if (reverse) {
         Chat.log(Chat.createTextBuilder().append("[").withColor(color.script.bracket)
             .append(ScriptName).withColor(color.script.name)
-            .append("]").withColor(color.script.bracket).append(" enabled").withColor(0,255,0)
+            .append("]").withColor(color.script.bracket).append(" enabled").withColor(0, 255, 0)
             .build());
     } else {
         Chat.log(Chat.createTextBuilder().append("[").withColor(color.script.bracket)
@@ -37,22 +30,22 @@ function ToggleScript(ScriptName, callback,color) {
 }
 
 //プレイヤーが認識しているブロックを取得する関数
-function getTargetedBlock(){
+function getTargetedBlock() {
     const targetblock = Player.getInteractionManager()?.getTargetedBlock();
-    if(targetblock){
-        const blockdata = World.getBlock(targetblock?.getX(),targetblock?.getY(),targetblock?.getZ());
+    if (targetblock) {
+        const blockdata = World.getBlock(targetblock?.getX(), targetblock?.getY(), targetblock?.getZ());
         return blockdata;
     }
 }
 
 //ブロックのstateを取得しJSONに整形しreturnする関数
-function GetBlockState(){
-    if(getTargetedBlock()){
+function GetBlockState() {
+    if (getTargetedBlock()) {
         const keys = getTargetedBlock()?.getBlockState().keySet().toArray();
         const values = getTargetedBlock()?.getBlockState().values().toArray();
-        
+
         let obj = {};
-        for(const valueindex in values){
+        for (const valueindex in values) {
             //@ts-ignore
             obj[keys[valueindex]] = values[valueindex]
         }
